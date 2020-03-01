eb5a706f02b44e7b95667d01ff70df71a03c2496
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-03-01 10:44:15 +0100

    2

--- 03-herencia/clases.php
+++ 03-herencia/clases.php
@@ -1,95 +1,96 @@
 <?php
 
-//HERENCIA: La posibilidad de compartir atributos y métodos entre clases
+// HERENCIA: la posibilidad de compartir atributos y métodos entre clases
 
-class Persona{
+class Persona {
+	
+	public $nombre;
+	public $apellidos;
+	public $altura;
+	public $edad;
+	
+	public function getNombre() {
+		return $this->nombre;
+	}
 
-    public $nombre;
-    public $apellidos;
-    public $altura;
-    public $edad;
+	public function getApellidos() {
+		return $this->apellidos;
+	}
 
-    public function getNombre(){
-        return $this->nombre;
-    }
+	public function getAltura() {
+		return $this->altura;
+	}
 
-    public function getApellidos(){
-        return $this->apellidos;
-    }
+	public function getEdad() {
+		return $this->edad;
+	}
 
-    public function getAltura(){
-        return $this->altura;
-    }
+	public function setNombre($nombre) {
+		$this->nombre = $nombre;
+	}
 
-    public function getEdad(){
-        return $this->edad;
-    }
+	public function setApellidos($apellidos) {
+		$this->apellidos = $apellidos;
+	}
 
-    public function setNombre($nombre){
-        $this->nombre = $nombre;
-    }
+	public function setAltura($altura) {
+		$this->altura = $altura;
+	}
 
-    public function setApellidos($apellidos){
-        $this->nombre = $nombre;
-    }
+	public function setEdad($edad) {
+		$this->edad = $edad;
+	}
 
-    public function setAltura($altura){
-        $this->altura = $altura;
-    }
-
-    public function setEdad($edad){
-        $this->edad = $edad;
-    }
-
-    public function hablar(){
-        return "Estoy hablando";
-    }
-
-    public function caminar(){
-        return "Estoy caminando";
-    }
+	public function hablar(){
+		return "Estoy hablando";
+	}
+	
+	public function caminar(){
+		return "Estoy caminando";
+	}
 }
 
 class Informatico extends Persona{
+	
+	public $lenguajes;
+	public $experienciaProgramador;
+	
+	public function __construct() {
+		$this->lenguajes = "HTML, CSS y JS";
+		$this->experienciaProgramador = 10;
+	}
 
-    public $lenguajes;
-    public $experienciaProgramador;
-
-    public function __construct(){
-        $this->lenguajes = "HTML, CSS Y JS";
-        $this->experienciaProgramador = 10;
-    }
-
-    public function sabeLenguajes($lenguajes){
-        $this->lenguajes = $lenguajes;
-
-        return $this->lenguajes;
-    }
-
-    public function programar(){
-        return "Soy programador";
-    }
-
-    public function repararOrdenador(){
-        return "Reparar ordenadores";
-    }
-
-    public function hacerOfimatica(){
-        return "Estoy escribiendo en Word";
-    }
-
-
+	public function sabeLenguajes($lenguajes){
+		$this->lenguajes = $lenguajes;
+		
+		return $this->lenguajes;
+	}
+	
+	public function programar(){
+		return "Soy programador";
+	}
+	
+	public function repararOrdenador(){
+		return "Reparar ordenadores";
+	}
+	
+	public function hacerOfimatica(){
+		return "Estoy escribiendo en word";
+	}
 }
 
 class TecnicoRedes extends Informatico{
-    public $auditarRedes;
-    public $experienciaRedes;
+	public $auditarRedes;
+	public $experienciaRedes;
+	
+	public function __construct() {
+		parent::__construct();
+		
+		$this->auditarRedes = 'experto';
+		$this->experienciaRedes = 5;
+	}
 
-    public function getauditarRedes(){
-        return $this->auditarRedes;
-    }
-
-    public function setauditarRedes($auditarRedes){
-        $this->auditarRedes = $auditarRedes;
-    }
+	public function auditoria(){
+		return "Estoy auditando una red";
+	}
 }
--- 03-herencia/index.php
+++ 03-herencia/index.php
@@ -5,10 +5,7 @@ $persona = new Persona();
 var_dump($persona);
 
 $informatico = new Informatico();
- 
-
 var_dump($informatico);
 
 $tecnico = new TecnicoRedes();
-$tecnico->setauditarRedes(2);
 var_dump($tecnico);
